ebe1c0df394849e831f2d646e169773bc6af5f02
Yonipineda <yonipineda1010@icloud.com>
2020-07-22 01:32:54 -0400

    curr work

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,12 +7,20 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.address = [0] * 256 # where an 8-bit is stored 
+        self.reg = [0b0] * 8 # 8-bit register 
+        self.ir = None # `IR`: Instruction Register, contains a copy of the currently executing instruction
+        self.mar = None  # `MAR`: Memory Address Register, holds the memory address we're reading or writing
+        self.mdr = None # `MDR`: Memory Data Register, holds the value to write or the value just read
+        self.fl = None # `FL`: Flags, see below
+        self.pc = 0 # program counter: address of current executing counter 
+        self.spl = None # stack pointer location 
+        self.ram = [0b0] * 0xF4
 
-    def load(self):
+    def load(self, filename: str):
         """Load a program into memory."""
 
-        address = 0
+        
 
         # For now, we've just hardcoded a program:
 
@@ -26,9 +34,21 @@ class CPU:
             0b00000001, # HLT
         ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            with open(filename, 'r') as file:
+                lines = (line for line in file.readlines() if not (line[0]=='#' or line[0]=='\n'))
+                program = [int(line.split('#')[0].strip(), 2) for line in lines]
+
+            address = 0
+
+
+            for instruction in program:
+                self.ram[address] = instruction
+                address += 1
+
+        except FileNotFoundError as error:
+            print(error)
+            sys.exit()
 
 
     def alu(self, op, reg_a, reg_b):
@@ -63,3 +83,10 @@ class CPU:
     def run(self):
         """Run the CPU."""
         pass
+
+    
+    def ram_read(self, mar):
+        return self.ram[mar] 
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = self.mdr
